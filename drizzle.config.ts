d00e6a526a2b13c938a534d763a339b3a5ff8834
Guilherme Thomas da Costa <guilhermethomas.dev@gmail.com>
2024-12-05 14:33:34 -0300

    criando repositorio para ferramenta web de controle de frota de veiculos

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'turso',
   dbCredentials: {
-    url: "libsql://pro-saude-database-pro-saude.aws-us-east-1.turso.io",
-    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzMyMjc2MzksImlkIjoiMDlhNTNhNTctODA1NS00NzNmLTk5ZTMtNmRmYzcwNmQ1NTY1In0.CUPfFLUF2WHFbd6Bw66mdSNE8h3csLMeEIg9Fzqvy1HXiecTZzI3NyWCUpr5DawCZXiJ1bzI4e-tZzySG_C2AA",
+    url: process.env.NEXT_PUBLIC_TURSO_DATABASE_URL!,
+    authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN!,
   },
 });
